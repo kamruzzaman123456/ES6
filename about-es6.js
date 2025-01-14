89ca7f60bdabcd17b78f15d1dec1ea7873095c70
kamruzzaman123456 <mdkamruzzamanmajumder@gmail.com>
2025-01-14 12:49:32 +0600

    first commit

---
+++ about-es6.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    // const result = a + b;
+    //  return result;
+     return a + b ;
+}
+
+// function expration ES5
+
+const add2 = function(a,b){
+    return a + b ;
+}
+
+// arrow function ES6
+
+const add3 = (a,b) => a +b 
+
+const sum = add3(23,34);
